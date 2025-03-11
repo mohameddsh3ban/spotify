4dfc2e0054ce22fed653811dd311ce59aeaa73ef
mohameddsh3ban <60802643+mohameddsh3ban@users.noreply.github.com>
2025-03-11 02:43:29 +0200

    note added

--- src/enviroments/environment.ts
+++ src/enviroments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
     production: true,
     Client_ID :"21485dcf2ab14dce9094c1606105302b",
     Client_Secret : "bb15c3a9af9b437b8eaed486acf638fe",
-    Redirect_URI : "http://xiii-voxpulse.vercel.app/callback"
-    // https://spotify-six-lac.vercel.app/callback
+    Redirect_URI : "https://xiii-voxpulse.vercel.app/callback"
+   // https://xiii-voxpulse.vercel.app/callback
   };
